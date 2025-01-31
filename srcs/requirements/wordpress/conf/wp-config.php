7a5b9347e8984a23ac8cee94c5b2a1ae92631d1d
wuluk22 <142003739+wuluk22@users.noreply.github.com>
2025-01-31 23:57:29 +0100

    Add files via upload

--- srcs/requirements/wordpress/conf/wp-config.php
+++ srcs/requirements/wordpress/conf/wp-config.php
@@ -85,7 +85,6 @@ $table_prefix = 'wp_';
  *
  * @link https://developer.wordpress.org/advanced-administration/debug/debug-wordpress/
  */
-define( 'WP_DEBUG', false );
 
 /* Add any custom values between this line and the "stop editing" line. */
 
